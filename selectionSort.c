df33870895b526bbc2785306d216c57c3c3bb952
Harsh00hs <83810596+Harsh00hs@users.noreply.github.com>
2025-01-29 12:29:56 +0530

    correct the selectionSort

--- selectionSort.c
+++ selectionSort.c
@@ -2,12 +2,16 @@
 
 void selectionSort(int arr[100], int n){
     for(int i=0; i<n-1; i++){
-        for(int j=i+1; j<n; j++){
-            if(arr[i]>arr[j]){
-                int temp=arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
+        int min = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
             }
+        }        
+        if (min != i) {
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
         }
     }
 }
